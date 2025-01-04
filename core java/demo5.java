448816cbbaa493d8bce5ac55630090779f01f969
venu9906 <venu990623@gmail.com>
2025-01-04 15:38:53 +0530

    Add files via upload
    WHILE

---
+++ core java/demo5.java
@@ -0,0 +1,10 @@
+public class demo5{
+    public static void main(String[] args){
+        String username = "venu";
+        int password = 123;
+        while((username == "venu") && (password == 123)){
+            System.out.println("you are logged in...");
+            break;
+        }
+    }
+}
