1b255d27deaba530b51db32934676dfce8404b49
venu9906 <venu990623@gmail.com>
2025-01-04 15:42:51 +0530

    Add files via upload

---
+++ core java/demo5_1.java
@@ -0,0 +1,10 @@
+public class demo5_1 {
+    public static void main(String[] args){
+        String username = "venu";
+        int password = 123;
+        while((username == "venu") && (password == 123)){
+            System.out.println("you are a student of mallareddy university...");
+            break;
+        }
+    }
+}
