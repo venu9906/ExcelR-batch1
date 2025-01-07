efb016939ce30675362d056e0f695d50850de4b4
venu9906 <venu990623@gmail.com>
2025-01-07 13:02:34 +0530

    Add files via upload

---
+++ core java/demo015.java
@@ -0,0 +1,16 @@
+public class demo015 {
+    public static void main(String[] args) {
+        int x=10;
+        int y=15;
+        System.out.println("x="+x);
+        System.out.println("x="+y);
+
+        int temp;
+        temp=x;
+        x = y;
+        y = temp;
+        System.out.println("x=" + x);
+        System.out.println("x=" + y);
+
+    }
+}
