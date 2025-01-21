d0600f75c2edf59a830e4a6786442fde64076802
venu9906 <venu990623@gmail.com>
2025-01-21 19:59:59 +0530

    greatest of 3 numbers

---
+++ core java/Demo003.java
@@ -0,0 +1,17 @@
+public class Demo003 {
+    public static void main(String[] args) {
+        int num1, num2, num3;
+        num1 = 10;
+        num2 = 20;
+        num3 = 30;
+        if((num1 > num2) && (num1 > num3)){
+            System.out.println("num1 is greater");
+        }
+        else if((num2 > num1) && (num2 > num3)){
+            System.err.println("num2 is greater");
+        }
+        else{
+            System.err.println("num3 is greater");
+        }
+}
+}
