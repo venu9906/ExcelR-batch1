c0e6271bbeb6d64e75e5e33566579286edb56472
venu9906 <venu990623@gmail.com>
2025-01-21 19:58:55 +0530

    greatest of two numbers

---
+++ core java/Demo002.java
@@ -0,0 +1,13 @@
+public class Demo002 {
+    public static void main(String[] args) {
+        int num1, num2;
+        num1 = 10;
+        num2 = 20;
+        if(num1 > num2){
+            System.out.println("num 1 is greater");
+        }
+        else{
+            System.out.println("num 2 is greater");
+        }
+    }
+}
