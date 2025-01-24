93a7af9d99591e4b11f4563b766e359dc2a92ff1
venu9906 <venu990623@gmail.com>
2025-01-24 18:56:02 +0530

    account

---
+++ core java/Demo009.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class Demo009 {
+    String Cust_Name = "Prasunamba";
+    long Account_no = 123456;
+    Double Balance = 10000.00;
+    void accept_details(){
+        System.out.println("enter ur name, account no");
+        Scanner sc = new Scanner(System.in);
+        Cust_Name = sc.nextLine();
+        Account_no = sc.nextLong();
+        // Balance = sc.nextDouble();
+    }
+    void Display_details(){
+        System.out.println("Hello "+Cust_Name);
+        System.out.println("ur balance is "+ Balance);
+    }
+    public static void main(String[] args){
+        Demo009 obj = new Demo009();
+        obj.accept_details();
+        obj.Display_details();
+    }
+}
