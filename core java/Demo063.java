20753b0c65529623da0d894a7f16d100832cf1ff
venu9906 <venu990623@gmail.com>
2025-02-10 23:07:52 +0530

    add 1st and last element of an array

---
+++ core java/Demo063.java
@@ -0,0 +1,8 @@
+public class Demo063 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+        int sum = arr[0]+arr[arr.length-1];
+
+        System.out.println("sum of 1st & last: "+sum);
+    }
+}
