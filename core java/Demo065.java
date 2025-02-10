9d1e9e4ec1f7733df38a85f85ecb6541640098ac
venu9906 <venu990623@gmail.com>
2025-02-10 23:09:15 +0530

    print array elements

---
+++ core java/Demo065.java
@@ -0,0 +1,9 @@
+public class Demo065 {
+    public static void main(String[] args) {
+        int[] array = {10,20,30,40};
+        System.out.println("array elements are: ");
+        for(int element : array){
+            System.out.println(element+" ");
+        }
+    }
+}
