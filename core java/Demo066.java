9ac2263370d2558cd6a873b96b694cf44f02ea11
venu9906 <venu990623@gmail.com>
2025-02-10 23:09:55 +0530

    index values of an array

---
+++ core java/Demo066.java
@@ -0,0 +1,10 @@
+public class Demo066 {
+    public static void main(String[] args) {
+        int[] array = {10,20,30,40};
+        System.out.println("Index values: ");
+        for (int i = 0; i < array.length-1; i++) {
+            System.out.println("Index " +i+ ":" +array[i]);
+            
+        }
+    }
+}
